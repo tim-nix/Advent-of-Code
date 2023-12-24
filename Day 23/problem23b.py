4e6f999bde6688f74016ea9d33daf74b61dd76c2
Tim Nix <tnix@uttyler.edu>
2023-12-24 13:01:30 -0600

    Advent of Code 2023 through Day 24

--- Day 23/problem23b.py
+++ Day 23/problem23b.py
@@ -1,3 +1,5 @@
+import time
+
 def readFile(filename):
    lines = []
    try:
@@ -17,100 +19,145 @@ def readFile(filename):
    return lines
 
 
-def processTrails(trails):
-   new_trails = []
-   for x in range(len(trails)):
-      new_trails.append([])
-      for y in range(len(trails[x])):
-         if trails[x][y] == '#':
-            new_trails[x].append('#')
-         else:
-            new_trails[x].append('.')
-
-   return new_trails
-
-
-# Find the longest hiking path by following the current
-# path as long as it does not branch.  If the path
-# branches, then recurse down each path and choose the
-# longest path.
-def findLongestHike(trails, current, stop, old_visited, top):
-   #print()
-   #print('current = ' + str(current))
-   #print('shortcuts = ' + str(shortcuts))
-      
-   # Make a copy of the visited set to allow for
-   # backtracking at higher recursion levels.
-   visited = set()
-   for val in old_visited:
-      visited.add(val)
-
-   # Test each possible neighbor location from the
-   # current location
+# From the trails map, extract all locations that are
+# intersections; that is, have either 3 or 4 non-tree
+# neighbors.  Add the start and stop locations to the
+# nodes collection.
+def buildNodes(trails, start, stop):
    deltas = [ (0, -1), (0, 1), (-1, 0), (1, 0) ]
-   toVisit = [ current ]
-   path = set()
-   last = current
-   # If there is only one path, then follow it.
-   while len(toVisit) == 1:
-      x, y = toVisit.pop(0)
-      #print('looking at ' + str((x, y)))
-      visited.add((x, y))
-      path.add((x, y))
+   
+   adjacencies = dict()
+   # Add the start node to the node collection and adjacencies.
+   nodes = [ start ]
+   adjacencies[start] = [ (start[0] + 1, start[1]) ]
+   # Iterate through each location on the trail map.
+   for x in range(len(trails)):
+      for y in range(len(trails[x])):
+         # If the current location is not a tree, then
+         # get each of its non-tree neighbors (n, s, e, w).
+         if trails[x][y] != '#':
+            adjacents = []
+            for delta_x, delta_y in deltas:
+               if ((0 <= (x + delta_x) < len(trails)) and (0 <= (y + delta_y) < len(trails[x]))):
+                  if trails[x + delta_x][y + delta_y] != '#':
+                     adjacents.append((x + delta_x, y + delta_y))
+
+            # If the node has more than two neighbors, then
+            # add it to the collection.  Also, add the
+            # neighbors to the adjacency dictionary.
+            if len(adjacents) > 2:
+               nodes.append((x, y))
+               adjacencies[(x, y)] = tuple(adjacents)
+
+   # Also add the stop node to the collection and adjacencies.
+   nodes.append(stop)
+   adjacencies[stop] = [ (stop[0] - 1, stop[1]) ]
+   
+   return (nodes, adjacencies)
+
+
+# Build a list of paths between the nodes; that is,
+# determine if a path exists between two nodes and
+# determine the associated distance.  If a path
+# exists, add it to the list of shortcuts.
+def buildPaths(trails, nodes, adjacencies):
+   shortcuts = []
+   deltas = [ (0, -1), (0, 1), (-1, 0), (1, 0) ]
+   finish = set(nodes)
+   # Look through all nodes.
+   while len(nodes) > 0:
+      start = nodes.pop(0)
+      visited = set()
+      visited.add(start)
+      # Follow a path from each adjacent neighbor to
+      # another node.  Add the two endpoints and the
+      # distance to shortcuts.
+      for neighbor in adjacencies[start]:
+         #print('neighbor = ' + str(neighbor))
+         distance = 1
+         current = neighbor
+         # Keep following the path as long as it is
+         # not at an intersection.
+         while (current not in finish):
+            x, y = current
+            visited.add((x, y))
+            distance += 1
+            toVisit = []
+            for delta_x, delta_y in deltas:
+               if ((0 <= (x + delta_x) < len(trails)) and
+                   (0 <= (y + delta_y) < len(trails[x]))):
+                  if (((x + delta_x, y + delta_y) not in visited) and
+                      (trails[x + delta_x][y + delta_y] != '#')):
+                     toVisit.append((x + delta_x, y + delta_y))
+
+            # Update the location
+            current = toVisit[0]
+
+         # Once the next node is reached, add the
+         # shortcut to the collection.
+         shortcuts.append((start, current, distance))
+
+   return shortcuts       
+
+
+# Build an adjacency matrix for the graph. The graph
+# is weighted, non-directed. The weights are the values
+# found in shortcuts. The function also returns the
+# index values for the start and step locations.
+def buildGraph(shortcuts, adjacencies, start, stop):
+   # The adjacencies dictionary keys provide the
+   # mapping from (x, y) location to the index into
+   # the adjacency matrix.
+   nodes = list(adjacencies.keys())
+
+   # Create an empty adjacency matrix of correct size.
+   graph = [ [ 0 for y in range(len(nodes)) ] for x in range(len(nodes)) ]
+
+   # Map each shortcut to the adjacency matrix
+   for v1, v2, d in shortcuts:
+      x = nodes.index(v1)
+      y = nodes.index(v2)
+      graph[x][y] = d
+      graph[y][x] = d
+
+   # Determine the start and stop indices
+   source = nodes.index(start)
+   sink = nodes.index(stop)
+
+   return (source, sink, graph)
+
+
+# Perform a depth-first search from the source
+# to the sink to determine the longest path.
+def dfs(graph, v1, sink, distance, old_visited):
+   # Since the set of visited nodes differs from
+   # path to path, we must make a copy of it so
+   # that modifications don't affect other paths.
+   visited = old_visited.copy()
+   visited.add(v1)
+
+   # If the current node is the sink, then end
+   # this path.
+   if v1 == sink:
+      return distance
+
+   # Follow each path from the current node, and
+   # save the distance of that path.
+   distances = [ 0 ]
+   for v2 in range(len(graph[v1])):
+      if (graph[v1][v2] > 0) and (v2 not in visited):
+         new_distance = distance + graph[v1][v2]
+         # Recurse along the path through each neighbor
+         distances.append(dfs(graph, v2, sink, new_distance, visited))
+
+   # Return the longest distance from this node
+   # to the sink.
+   return max(distances)
       
-      if (x, y) == stop:
-         #print('found the end')
-         return path
 
-      elif ((x, y) in shortcuts) and (shortcuts[(x, y)][0] not in visited):
-         path_end, sc_path = shortcuts[(x, y)]
-         #print('shortcut found from ' + str((x, y)) + ' to ' + str(path_end))
-         toVisit.append(path_end)
-         path = path.union(sc_path)
-
-      else:
-         for delta_x, delta_y in deltas:
-            if ((0 <= (x + delta_x) < len(trails)) and
-                (0 <= (y + delta_y) < len(trails[x]))):
-               if (((x + delta_x, y + delta_y) not in visited) and
-                   (trails[x + delta_x][y + delta_y] != '#')):
-                  toVisit.append((x + delta_x, y + delta_y))
-
-      if len(toVisit) == 1:
-         last = (x, y)
-
-   # If there are no more paths, then return to the
-   # higher recursion level
-   if len(toVisit) == 0:
-      #print('dead end at ' + str((x, y)) + '; returning 0')
-      return set()
-
-   if (len(path) > 1) and (current not in shortcuts):
-      #print('adding shortcut from ' + str(current) + ' to ' + str(last))
-      shortcuts[current] = (last, path.copy())
-      shortcuts[last] = (current, path.copy())
-      
-   # If there are multiple paths from the current location,
-   # then follow each recursively.  Afterwards, choose the
-   # longest path and return it.
-   from_here = set()
-   #print('toVisit = ' + str(toVisit))
-   if top:
-      print('at the top toVisit = ' + str(toVisit))
-      
-   for p in toVisit:
-      if top:
-         print('following path ' + str(p))
-      new_path = findLongestHike(trails, p, stop, visited, False)
-      if len(new_path) > len(from_here):
-         from_here = new_path
-
-   return path.union(from_here)
-
-
-shortcuts = dict()
-
+   
 if __name__ == '__main__':
+   start_time = time.time()
    # Read in the file and organize the data
    trails = readFile("input23b.txt")
 
@@ -118,20 +165,17 @@ if __name__ == '__main__':
    start = (0, 1)
    stop = (len(trails) - 1, len(trails) - 2)
 
-   #print('process trails')
-   # Get rid of steep slopes
-   #trails = processTrails(trails)
-   
-   print('find longest path')
-   # Find the longest hiking path
-   path = findLongestHike(trails, start, stop, set(), True)
+   print('building nodes')
+   nodes, adjacencies = buildNodes(trails, start, stop)
 
-   #print(path)
-   #print('len(path) = ' + str(len(path)))
+   print('building paths')
+   shortcuts = buildPaths(trails, nodes, adjacencies)
 
-   # Print out the resulting distance
-   print('distance = ' + str(len(path) - 1))
-   
-   
-   
+   print('building graph')
+   source, sink, graph = buildGraph(shortcuts, adjacencies, start, stop)
+
+   print('calculating distance')
+   distance = dfs(graph, source, sink, 0, set())
+   print('distance = ' + str(distance))
+   print("\n\n--- %s seconds ---" % (time.time() - start_time))
    
