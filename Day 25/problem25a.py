983a9f79a91ee5eef201cc3a00f1c64aa52eaf18
Tim Nix <tnix@uttyler.edu>
2023-12-26 09:56:36 -0600

    Advent of Code 2023 through Day 25

--- Day 25/problem25a.py
+++ Day 25/problem25a.py
@@ -1,4 +1,6 @@
-from random import choice
+# This solution uses the networkx library to determine
+# the minimum cut set of the graph.
+import networkx
 
 def readFile(filename):
    lines = []
@@ -19,7 +21,7 @@ def readFile(filename):
    return lines
 
 
-
+# From the file input, generate an adjacency list.
 def parseInput(lines):
    graph = dict()
    for line in lines:
@@ -29,111 +31,44 @@ def parseInput(lines):
          graph[node] = edges
       else:
          graph[node].extend(edges)
-         
-      for edge in edges:
-         if edge not in graph:
-            graph[edge] = [ node ]
-         else:
-            graph[edge].append(node)
 
-   nodes = list(graph.keys())
-   
-   am = [ [ 0 for j in range(len(nodes)) ] for i in range(len(nodes)) ]
-   for i in range(len(nodes)):
-      for n in graph[nodes[i]]:
-         j = nodes.index(n)
-         am[i][j] = 1
-         am[j][i] = 1
-
-   return am
+   return graph
 
 
-def genEdges(graph):
-   edges = set()
 
-   for i in range(len(graph) - 1):
-      for j in range(i + 1, len(graph[i])):
-         if graph[i][j] > 0:
-            edges.add((i, j))
-
-   return list(edges)
-
-
-def findCut(graph):
-   edges = genEdges(graph)
-   for i in range(len(edges) - 2):
-      print('testing ' + str(i) + ' of ' + str(len(edges) - 2))
-      # remove edge1
-      graph[edges[i][0]][edges[i][1]] = 0
-      graph[edges[i][1]][edges[i][0]] = 0
-
-      for j in range(i + 1, len(edges) - 1):
-         # remove edge1
-         graph[edges[j][0]][edges[j][1]] = 0
-         graph[edges[j][1]][edges[j][0]] = 0
-
-         for k in range(j + 1, len(edges)):
-            # if you disconnect the wire between hfx/pzl,
-            # the wire between bvb/cmg, and the wire
-            # between nvd/jqt,
-               
-            graph[edges[k][0]][edges[k][1]] = 0
-            graph[edges[k][1]][edges[k][0]] = 0
-            
-            # test if connected
-            sizes = getComponentSizes(graph)
-               
-            if (len(sizes) == 2) and (1 not in sizes):
-               print('component sizes = ' + str(sizes))
-               print('result = ' + str(sizes[0] * sizes[1]))
-               return
-
-            # add edges back
-            graph[edges[k][0]][edges[k][1]] = 1
-            graph[edges[k][1]][edges[k][0]] = 1
-
-         # add edges back
-         graph[edges[j][0]][edges[j][1]] = 1
-         graph[edges[j][1]][edges[j][0]] = 1
-
-      # add edges back
-      graph[edges[i][0]][edges[i][1]] = 1
-      graph[edges[i][1]][edges[i][0]] = 1
-
-   print('Nothing found.')
-
-
-def getComponentSizes(graph):
-   nodes = [ i for i in range(len(graph)) ]
-   sizes = []
-   
-   while len(nodes) > 0:
-      toVisit = [ nodes[0] ]
-      visited = set()
-      visited.add(nodes[0])
-      nodes.pop(0)
-      while len(toVisit) > 0:
-         current = toVisit.pop(0)
-         for neighbor in range(len(graph[current])):
-            if (graph[current][neighbor] > 0) and (neighbor not in visited):
-               toVisit.append(neighbor)
-               visited.add(neighbor)
-               if neighbor in nodes:
-                  nodes.remove(neighbor)
-
-      sizes.append(len(visited))
-
-   return sizes
 
 if __name__ == '__main__':
    # Read in the file and organize the data.
    lines = readFile("input25b.txt")
 
+   # Generate the adjacency list (dictionary)
    graph = parseInput(lines)
 
-   # Generate the graph split into two
+   # Create a networkx graph.
+   lib_graph = networkx.Graph()
+
+   # For each edge in the adjacency list,
+   # add two edges to the graph since it
+   # is not directed.
+   for node1 in graph:
+      for node2 in graph[node1]:
+         lib_graph.add_edge(node1, node2)
+         lib_graph.add_edge(node2, node1)
+
+   # Determine the minimum cut edges.
+   min_cut = networkx.minimum_edge_cut(lib_graph)
+
+   # Remove those edges from the graph.
+   lib_graph.remove_edges_from(min_cut)
+
+   # Generate the two resulting components.
+   components = list(networkx.connected_components(lib_graph))
+
+   # The answer is the product of the sizes of the two
    # components.
-   graph = findCut(graph)
+   print('result = ' + str(len(components[0]) * len(components[1])))
+         
+         
 
    
    
--- Day 25/problem25b.py
+++ Day 25/problem25b.py
@@ -1,4 +1,7 @@
-import networkx
+# This solution uses Karger's algorithm to determine
+# the minimum cut set of the graph.
+import random
+import time
 
 def readFile(filename):
    lines = []
@@ -20,54 +23,135 @@ def readFile(filename):
 
 
 # From the file input, generate an adjacency list.
+# However, in this case, each stored edge is a tuple
+# containing both vertices of the edge.
 def parseInput(lines):
    graph = dict()
    for line in lines:
       node, edges = line.split(':')
       edges = edges.lstrip().split(' ')
       if node not in graph:
-         graph[node] = edges
-      else:
-         graph[node].extend(edges)
+         graph[node] = []
+         
+      for edge in edges:
+         graph[node].append((node, edge))
+         if edge not in graph:
+            graph[edge] = [ (edge, node) ]
+         else:
+            graph[edge].append((edge, node))
 
    return graph
 
 
+# Repeatedly contract two nodes into a single node
+# until only two nodes remain. The edges between
+# the two nodes constitute the cut set.
+def contract(graph1):
+   # Make a deep copy of the graph. We need to do
+   # this to preserve the original graph as we
+   # may need to execute this function repeatedly
+   # until the min cut set is generated.
+   print('copying graph')
+   graph2 = dict()
+   for key in graph1:
+      for value in graph1[key]:
+         if key not in graph2:
+            graph2[key] = [ value ]
+         else:
+            graph2[key].append(value)
+
+   print('contracting')
+   # Keep contracting until only two nodes remain
+   while len(graph2.keys()) > 2:
+      # Generate list of nodes
+      nodes = list(graph2.keys())
+      
+      # Generate list of edges from the current set
+      # of nodes. This set of edges will allow the
+      # uniform, random selection of an edge from
+      # the collection of edges.
+      edges = []
+      for key in graph2:
+         for edge1, edge2 in graph2[key]:
+            if edge1 not in key:
+               search_edge = edge1
+            elif edge2 not in key:
+               search_edge = edge2
+
+            possibles = [ nodes[i] for i in range(len(nodes)) if search_edge in nodes[i] ]
+            if len(possibles) > 0:
+               found_edge = possibles[0]
+            
+         edges.append((key, found_edge))
+
+      # Select a random edge.
+      node1, node2 = random.choice(edges)
+
+      # Combine the two nodes of the edge into a single node.
+      # Separate the names by a comma to prevent mismatching
+      # of the original node labels with the concatenation
+      # of the newnode label (e.g., 'ssl' in 'qrs,slp' vs
+      # 'ssl' in 'qrsslp').
+      newnode = node1 + ',' + node2
+      graph2[newnode] = []
+      
+      # Iterate through the list of edges from node1, and if
+      # it is not a self-loop, then add it as an edge to
+      # newnode.
+      for edge1, edge2 in graph2[node1]:
+         if (edge1 not in newnode) and (edge2 not in newnode):
+            print('neither end is rooted')
+         elif (edge1 in newnode) and (edge2 not in newnode):
+            graph2[newnode].append((edge1, edge2))
+         elif (edge2 in newnode) and (edge1 not in newnode):
+            graph2[newnode].append((edge2, edge1))
+
+      # Delete node1 from the graph.
+      del graph2[node1]
+
+      # Iterate through the list of edges from node2, and if
+      # it is not a self-loop, then add it as an edge to
+      # newnode.
+      for edge1, edge2 in graph2[node2]:
+         if (edge1 not in newnode) and (edge2 not in newnode):
+            print('neither end is rooted')
+         elif (edge1 in newnode) and (edge2 not in newnode):
+            graph2[newnode].append((edge1, edge2))
+         elif (edge2 in newnode) and (edge1 not in newnode):
+            graph2[newnode].append((edge2, edge1))
+
+      # Delete node2 from the graph.
+      del graph2[node2]
+
+   # Return the resulting graph.
+   return graph2
+      
 
 
 if __name__ == '__main__':
+   start_time = time.time()
    # Read in the file and organize the data.
    lines = readFile("input25b.txt")
-
-   # Generate the adjacency list (dictionary)
    graph = parseInput(lines)
 
-   # Create a networkx graph.
-   lib_graph = networkx.Graph()
+   # Since we know that the size of the cut set is 3,
+   # repeat the process until a cut set of size 3 is
+   # found.
+   done = False
+   while not done:
+      graph2 = contract(graph)
+      nodes = list(graph2.keys())
+      if len(graph2[nodes[0]]) == 3:
+         done = True
+         length1 = len(nodes[0].replace(',', '')) // 3
+         length2 = len(nodes[1].replace(',', '')) // 3
+         print('results = ' + str(length1 * length2))
 
-   # For each edge in the adjacency list,
-   # add two edges to the graph since it
-   # is not directed.
-   for node1 in graph:
-      for node2 in graph[node1]:
-         lib_graph.add_edge(node1, node2)
-         lib_graph.add_edge(node2, node1)
-
-   # Determine the minimum cut edges.
-   min_cut = networkx.minimum_edge_cut(lib_graph)
-
-   # Remove those edges from the graph.
-   lib_graph.remove_edges_from(min_cut)
-
-   # Generate the two resulting components.
-   components = list(networkx.connected_components(lib_graph))
-
-   # The answer is the product of the sizes of the two
-   # components.
-   print('result = ' + str(len(components[0]) * len(components[1])))
-         
+   print("\n\n--- %s seconds ---" % (time.time() - start_time))
          
 
+
+   
    
    
    
