b9b20e9ca9c21768d73699e0e3311abdb5268278
Tim Nix <tnix@uttyler.edu>
2023-12-30 12:03:54 -0600

    Advent of Code 2023 through Day 25

--- Day 05/problem05a.py
+++ Day 05/problem05a.py
@@ -1,3 +1,12 @@
+# For each seed value, map the seed through the
+# first mapping data, take the output and apply
+# it to the next mapping data, etc. until the
+# final value is calculated.  From the collection
+# of values generated from the seeds, display
+# the smallest value.
+
+# Read in the data file and convert it to a list
+# of strings.
 def readFile(filename):
    lines = []
    try:
@@ -17,7 +26,12 @@ def readFile(filename):
    return lines
 
 
+# From the line of data input that contains the
+# seed values (represented as a string), extract
+# each value and return it as a list of integers.
 def get_seeds(line):
+   # The 'seeds' label is separated from the seed
+   # values by a colon ':'.
    seeds = line.split(':')
    seeds = seeds[1].split()
    seed_vals = [ int(seed) for seed in seeds ]
@@ -25,6 +39,13 @@ def get_seeds(line):
    return seed_vals
    
 
+# From a line of data that contains a mapping
+# from one type of condition to another (e.g.,
+# seed-to soil map').  Each line within a map
+# contains three numbers: the destination range
+# start, the source range start, and the range
+# length.  Return these values as a tuple of
+# integers.
 def get_map(line):
    values = line.split()
    dst = int(values[0])
@@ -36,35 +57,62 @@ def get_map(line):
 
 if __name__ == '__main__':
    lines = readFile("input5b.txt")
+   # Extract the seed data from the first line
+   # of input.
    seeds = get_seeds(lines[0])
 
-   print('Generating mappings.')
+   # Generate the mappings.
+   #print('Generating mappings.')
    map_set = []
    mapping = []
    for i in range(1, len(lines)):
       line = lines[i]
+      # Different mappings are separated by empty
+      # lines of string (i.e., '').
       if line == '':
+         # If an empty line is encountered but it
+         # is not the first empty line, then add
+         # the mapping to the map_set.
          if len(mapping) > 0:
             map_set.append(mapping)
+      # The first line of the mapping information
+      # is the title label of the specific mapping.
+      # We ignore the label and prepare for the
+      # lines of data that follow it.
       elif 'map' in line:
          mapping = []
+      # If the data is mapping data, convert it to
+      # a tuple of integers and add it to the list
+      # of mapping data.
       else:
          mapping.append(get_map(line))
 
+   # Add the last round of mapping data to the
+   # map_set.
    map_set.append(mapping)
 
-   print('Generating locations.')
+   # For each seed value, find the lowest location
+   # number that corresponds to any of the initial
+   # seeds. To do this, you'll need to convert each
+   # seed number through other categories until you
+   # can find its corresponding location number.
+   #print('Generating locations.')
    locations = []
    for s in seeds:
       t = s
+      # Within each map set:
       for m1 in map_set:
-         found = False
-         for m2 in m1:
-            if not found and (t >= m2[0]) and (t < m2[0] + m2[2]):
-               found = True
-               t = m2[1] + (t - m2[0])
+         # Find the correct starting range, calculate the
+         # offset, and then calculate the corresponding
+         # destination value.
+         for src, dst, rng in m1:
+            if (t >= src) and (t < src + rng):
+               t = dst + (t - src)
+               break
 
       locations.append(t)
 
+   # Find the lowest location number that corresponds
+   # to any of the initial seed numbers and display it.
    #print(locations)
    print('minimum location = ' + str(min(locations)))
--- Day 05/problem05b.py
+++ Day 05/problem05b.py
@@ -1,3 +1,12 @@
+# Apply each mapping (one at a time) to the list of
+# seed ranges and build a new collection of ranges
+# based off of the mapping. Repeat until all mappings
+# are applied. Find the smallest lower bound among
+# the completed mappings and display it.
+
+
+# Read in the data file and convert it to a list
+# of strings.
 def readFile(filename):
    lines = []
    try:
@@ -17,18 +26,36 @@ def readFile(filename):
    return lines
 
 
+# From the line of data input that contains the
+# seed values (represented as a string), extract
+# each pair of numbers. The first value is the
+# start of the range and the second value is the
+# length of the range. Return a list of tuples
+# with each tuple consisting of two integers;
+# the start of the range and the end of the
+# range (using the start of the range and the
+# length of the range to calculate the end).
 def get_seeds(line):
+   # The 'seeds' label is separated from the seed
+   # values by a colon ':'.
    seeds = line.split(':')
    seeds = seeds[1].split()
    seed_vals = [ int(seed) for seed in seeds ]
    seed_pairs = []
    for i in range(0, len(seed_vals), 2):
-      seed_pairs.append((seed_vals[i], seed_vals[i + 1]))
+      seed_pairs.append((seed_vals[i], seed_vals[i] + seed_vals[i + 1]))
       
 
    return seed_pairs
    
 
+# From a line of data that contains a mapping
+# from one type of condition to another (e.g.,
+# seed-to soil map').  Each line within a map
+# contains three numbers: the destination range
+# start, the source range start, and the range
+# length.  Return these values as a tuple of
+# integers.
 def get_map(line):
    values = line.split()
    dst = int(values[0])
@@ -38,105 +65,82 @@ def get_map(line):
    return (src, dst, rng)
 
 
-if __name__ == '__main__':
-   lower_bound = 0
-   upper_bound = 100000000000
-   
+if __name__ == '__main__':  
    lines = readFile("input5b.txt")
+   # Extract the seed data from the first line
+   # of input.
    seeds = get_seeds(lines[0])
 
-   print('Generating mappings.')
+   # Generate the mappings.
+   #print('Generating mappings.')
    map_set = []
    mapping = []
    for i in range(1, len(lines)):
       line = lines[i]
+      # Different mappings are separated by empty
+      # lines of string (i.e., '').
       if line == '':
+         # If an empty line is encountered but it
+         # is not the first empty line, then add
+         # the mapping to the map_set.
          if len(mapping) > 0:
             map_set.append(mapping)
+      # The first line of the mapping information
+      # is the title label of the specific mapping.
+      # We ignore the label and prepare for the
+      # lines of data that follow it.
       elif 'map' in line:
          mapping = []
+      # If the data is mapping data, convert it to
+      # a tuple of integers and add it to the list
+      # of mapping data.
       else:
          mapping.append(get_map(line))
 
+   # Add the last round of mapping data to the
+   # map_set.
    map_set.append(mapping)
 
-   seed_ranges = [ (s, s + r) for s, r in seeds ]
-   #print(seed_ranges)
-
-   print('Converting map.')
-   # convert map_set format and merge maps
-   locations = []
+   # Apply each map_set to the seeds:
+   count = 0
    for m1 in map_set:
-      start  = [ (lower_bound, upper_bound, 0) ]
-      for m2 in m1:
-         # convert (initial, mapped, range) to (lower, upper, offset)
-         lower = m2[0]
-         upper = m2[0] + m2[2] - 1
-         offset = m2[1] - lower
+      count += 1
+      #print('pass ' + str(count) + ' of ' + str(len(map_set)))
+      mapped_values = []
+      while len(seeds) > 0:
+         seed_lower, seed_upper = seeds.pop(0)
+         #print('seed = ' + str((seed_lower, seed_upper)))
+      
+         # Determine if the mapping applies, split the seed
+         # range, if necessary, and apply the mapping.
+         for src, dst, rng in m1:
+            #print('rule = ' + str((src, dst, rng)))
+            # Find the overlap, if any.
+            overlap_lower = max(seed_lower, src)
+            overlap_upper = min(seed_upper, src + rng)
+            # When the overlap is found, apply it to the
+            # applicable seed range.
+            if overlap_lower < overlap_upper:
+               mapped_values.append((dst + (overlap_lower - src), dst + (overlap_upper - src)))
+               # For the seed range outside of the overlap,
+               # return it to the pool of seeds for later
+               # mapping.
+               if overlap_lower > seed_lower:
+                  seeds.append((seed_lower, overlap_lower))
+               if seed_upper > overlap_upper:
+                  seeds.append((overlap_upper, seed_upper))
+               break
+         # Any source numbers that aren't mapped correspond to
+         # the same destination number, so if a mapping is not
+         # found, just reappend the seed range.
+         else:
+            mapped_values.append((seed_lower, seed_upper))
 
-         #print('inserting ' + str(m2))
-
-         new_locations = []
-         # check for overlaps
-         for minimum, maximum, shift in start:
-            #print('parsing ' + str((minimum, maximum, shift)))
-            if (lower <= minimum) and (upper >= maximum):
-               new_locations.append((minimum, maximum, shift + offset))
-            elif (lower == minimum) and (upper < maximum):
-               new_locations.append((minimum, upper, shift + offset))
-               new_locations.append((upper + 1, maximum, shift))
-            elif (lower > minimum) and (lower < maximum) and (upper >= maximum):
-               new_locations.append((minimum, lower - 1, shift))
-               new_locations.append((lower, maximum, shift + offset))
-            elif (lower > minimum) and (upper < maximum):
-               new_locations.append((minimum, lower - 1, shift))
-               new_locations.append((lower, upper, shift + offset))
-               new_locations.append((upper + 1, maximum, shift))
-            elif (lower < minimum) and (upper >= minimum) and (upper < maximum):
-               new_locations.append((minimum, upper, shift + offset))
-               new_locations.append((upper + 1, maximum, shift))
-            elif (upper < minimum) or (lower > maximum):
-               new_locations.append((minimum, maximum, shift))
-            elif (lower == maximum):
-               new_locations.append((minimum, maximum - 1, shift))
-               new_locations.append((maximum, maximum, shift + offset))
-            elif (upper == minimum):
-               new_locations.append((minimum, minimum, shift + offset))
-               new_locations.append((minimum + 1, maximum, shift))
-            else:
-               print('found unresolved case')
-               print('m2 = ' + str(m2))
-               print('location = ' + str((minimum, maximum, shift)))
-               print('lower = ' + str(lower) + ' and upper = ' + str(upper))
-               print('minimum = ' + str(minimum) + ' and maximum = ' + str(maximum))
-
-         start = new_locations
-      locations.append(start)
-      #print(locations)
-      #print()
-
-   print('Mapping seeds.')
-   final = []
-   round_num = 1
-   for min_seed, max_seed in seed_ranges:
-      print('Calculating round ' + str(round_num) + ' of ' + str(len(seed_ranges)) + ' rounds.')
-      round_num += 1
-      for i in range(min_seed, max_seed):
-         loc = i
-         #print('seed = ' + str(loc))
-         for mapping in locations:
-            for lower, upper, shift in mapping:
-               #print((lower, upper, shift))
-               if (loc >= lower) and (loc <= upper):
-                  #print('bounding found')
-                  loc = loc + shift
-                  #print('new value = ' + str(loc))
-                  break
+      seeds = mapped_values
             
-         final.append(loc)
-      print('smallest so far = ' + str(min(final)))
-
-   #print(final)
-   print(min(final))
+   # Find the lowest location number that corresponds
+   # to any of the initial seed numbers and display it.
+   #print(locations)
+   print('minimum location = ' + str(min(seeds)[0]))
          
                
